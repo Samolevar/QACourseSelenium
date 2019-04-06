30f3d62028aee086deff09c1062b638902364972
Денис Аминев <daminev@skbkontur.ru>
2019-04-06 23:03:25 +0500

    Initial application

--- QACourseSelenium/TestClass.cs
+++ QACourseSelenium/TestClass.cs
@@ -1,20 +1,33 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using System.Collections;
-using System.Collections.Generic;
+
+using System.Threading;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace QACourseSelenium
 {
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void TestMethod()
+        private ChromeDriver chromeDriver =  new ChromeDriver();
+
+        [OneTimeTearDown]
+        public void TearDown()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            chromeDriver.Quit();
+        }
+
+        [Test]
+        public void ChromeDriverTest()
+        {
+            chromeDriver.Navigate().GoToUrl("http://www.google.com");
+            chromeDriver.FindElement(By.ClassName("gLFyf")).SendKeys("google");
+            Thread.Sleep(500);
+            chromeDriver.FindElement(By.ClassName("gLFyf")).SendKeys(Keys.Enter);
+            var text = chromeDriver.FindElement(By.ClassName("e2BEnf")).Text;
+            Assert.That(text, Is.EqualTo("Главные новости").After(500));
         }
     }
 }
