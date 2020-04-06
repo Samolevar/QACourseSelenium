de39d7841f3f3b115f45027e16b67e096450056f
Marina <marina.marinam48@gmail.com>
2020-04-07 00:10:31 +0530

    Add tests in TestClass.cs

--- QACourseSelenium/TestClass.cs
+++ QACourseSelenium/TestClass.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace QACourseSelenium
 {
@@ -26,14 +27,247 @@ namespace QACourseSelenium
         }
 
         [Test]
-        public void ChromeDriverTest()
+        public void InputShortLogin()
         {
-            driver.Navigate().GoToUrl("http://www.google.com");
-            driver.FindElement(By.ClassName("gLFyf")).SendKeys("google");
-            Thread.Sleep(500);
-            driver.FindElement(By.ClassName("gLFyf")).SendKeys(Keys.Enter);
-            var text = driver.FindElement(By.ClassName("e2BEnf")).Text;
-            Assert.That(text, Is.EqualTo("Вместе с google часто ищут").After(500));
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwe");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("mail@mail.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 4 and 24 characters long.", result);
+        }
+
+        [Test]
+        public void InputLongLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("ItIsVeryVeryVeryVeryVeryVeryLongLogin");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("mail@mail.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 4 and 24 characters long.", result);
+        }
+
+        [Test]
+        public void InputLoginWithNumbers()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("123456");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("mail@mail.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("flashes")).Text;
+            Assert.AreEqual("Спасибо за регистрацию!", result);
+        }
+
+        [Test]
+        public void InputInvalidLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("!!!!!!");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("mail@mail.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Invalid input.", result);
+        }
+
+
+        [Test]
+        public void InputEmptyLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("m@l.r");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 6 and 35 characters long.\r\nInvalid email address.", result);
+        }
+
+        [Test]
+        public void InputEmptyEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("m@l.r");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 6 and 35 characters long.\r\nInvalid email address.", result);
+        }
+
+
+        [Test]
+        public void InputShortEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("m@l.r");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 6 and 35 characters long.\r\nInvalid email address.", result);
+        }
+
+        [Test]
+        public void InputLongEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("llllllllllllllllmesddddddddddddd@l.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Field must be between 6 and 35 characters long.", result);
+        }
+
+        [Test]
+        public void InputInvalidEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("qwerty");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Invalid email address.", result);
+        }
+
+        [Test]
+        public void InputDifferentPasswords()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("qwerty@m.ru");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("12345");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.AreEqual("Passwords must match", result);
+        }
+
+        [Test]
+        public void InputCorrectFields()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/ekb/form");
+            driver.FindElement(By.ClassName("student")).SendKeys("Чухлеб Марина");
+            driver.FindElement(By.ClassName("text-plain")).SendKeys("qwerty");
+
+            var select = new SelectElement(driver.FindElement(By.ClassName("selected")));
+            select.Options[1].Click();
+
+            driver.FindElement(By.ClassName("text-email")).SendKeys("qwerty@qwe.ty");
+            driver.FindElement(By.ClassName("text-password")).SendKeys("123456");
+            driver.FindElement(By.Id("confirm")).SendKeys("123456");
+
+            var checkbox = driver.FindElement(By.ClassName("button-checkbox"));
+            checkbox.Click();
+            checkbox.Submit();
+
+            var result = driver.FindElement(By.ClassName("flashes")).Text;
+            Assert.AreEqual("Спасибо за регистрацию!", result);
         }
     }
 }
