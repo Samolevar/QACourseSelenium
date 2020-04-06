d2ff51dc698c6f382c24f71f72d572a2abe8f042
aleksfr <volorfal@gmail.com>
2020-04-06 22:43:29 +0400

    Автоматизация Версия 2

--- QACourseSelenium/TestClass.cs
+++ QACourseSelenium/TestClass.cs
@@ -29,13 +29,13 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldAcceptValidData()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frolov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
-            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("aa@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
             var acceptTos = driver.FindElement(By.Id("accept_tos"));
@@ -48,14 +48,14 @@ namespace QACourseSelenium
         }
         
         [Test]
-        public void Form_ShouldAcceptLoginSpecSimvol()
+        public void Form_ShouldAcceptLoginSpecialSymbol()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
-            driver.FindElement(By.Id("username")).SendKeys("frolov@");
+            driver.FindElement(By.Id("username")).SendKeys("fro$lov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -71,12 +71,12 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldNotAcceptShortLogin()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("fro");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -92,12 +92,12 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldNotAcceptLoginDigits()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("1111");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -111,14 +111,14 @@ namespace QACourseSelenium
         }
 
         [Test]
-        public void Form_ShouldAcceptLogin4Digits()
+        public void Form_ShouldAcceptLoginMinNumbers()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frol");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -132,14 +132,14 @@ namespace QACourseSelenium
         }
         
         [Test]
-        public void Form_ShouldAcceptLogin24Digits()
+        public void Form_ShouldAcceptLoginMaxNumbers()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frrrrrrrrrrrrrrrrrrrrrrr");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -153,14 +153,14 @@ namespace QACourseSelenium
         }
         
         [Test]
-        public void Form_ShouldAcceptLogin25Digits()
+        public void Form_ShouldAcceptLongLogin()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frrrrrrrrrrrrrrrrrrrrrrrr");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -176,13 +176,55 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldAcceptSexFemale()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frolov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Female");
             driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+
+            var text = driver.FindElement(By.ClassName("flashes")).Text;
+            Assert.That(text == "Спасибо за регистрацию!");
+        }
+        
+        [Test]
+        public void Form_ShouldAcceptEmailMinNumbersName()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
+            driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
+            driver.FindElement(By.Id("username")).SendKeys("frolov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+
+            var text = driver.FindElement(By.ClassName("flashes")).Text;
+            Assert.That(text == "Спасибо за регистрацию!");
+        }
+        
+        [Test]
+        public void Form_ShouldAcceptEmailMaxNumbersName()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
+            driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
+            driver.FindElement(By.Id("username")).SendKeys("frolov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
             var acceptTos = driver.FindElement(By.Id("accept_tos"));
@@ -197,12 +239,12 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldAcceptEmailNotValid()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frolov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("aaa.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -218,12 +260,12 @@ namespace QACourseSelenium
         [Test]
         public void Form_ShouldAcceptEmailShortName()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frolov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
+            selectSex.SelectByText("Male");
             driver.FindElement(By.Id("email")).SendKeys("@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
             driver.FindElement(By.Id("confirm")).SendKeys("123");
@@ -237,25 +279,48 @@ namespace QACourseSelenium
         }
         
         [Test]
-        public void Form_ShouldAcceptConfirmPassword()
+        public void Form_ShouldAcceptEmailLongName()
         {
-            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
             driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
             driver.FindElement(By.Id("username")).SendKeys("frolov");
             var sex = driver.FindElement(By.Id("sex"));
             var selectSex = new SelectElement(sex);
-            selectSex.SelectByText("Мужской");
-            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@a.ru");
             driver.FindElement(By.Id("password")).SendKeys("123");
-            driver.FindElement(By.Id("confirm")).SendKeys("1234");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
             var acceptTos = driver.FindElement(By.Id("accept_tos"));
             acceptTos.Click();
 
             driver.FindElement(By.CssSelector("input[value=Register]")).Click();
 
             var error = driver.FindElement(By.ClassName("errors")).Text;
-            Assert.That(error == "Passwords must match");
-
+            Assert.That(error == "Field must be between 6 and 35 characters long.");
         }
+        
+        [Test]
+        public void Form_ShouldAcceptConfirmPassword()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/izh/form");
+            driver.FindElement(By.Id("student")).SendKeys("Frolov Aleksey");
+            driver.FindElement(By.Id("username")).SendKeys("frolov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("1234");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+         
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+         
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Passwords must match");
+         
+        }
+                 
+              
     }
 }
