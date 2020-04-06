4d0617c349f8f5edb88d63ebfc1df2e4fe6ba7b4
Соня <sonyasonya3004@gmail.com>
2020-04-06 21:03:47 +0400

    Автотесты

--- QACourseSelenium/TestClass.cs
+++ QACourseSelenium/TestClass.cs
@@ -5,10 +5,12 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace QACourseSelenium
 {
-    [TestFixture]
+    
+   [TestFixture]
     public class TestClass
     {
         private IWebDriver driver;
@@ -22,18 +24,179 @@ namespace QACourseSelenium
         [OneTimeTearDown]
         public void TearDown()
         {
-            driver.Quit();
+            // driver.Quit();
         }
 
+        
+       
         [Test]
-        public void ChromeDriverTest()
+        public void Form_ShouldAcceptValidData()
         {
-            driver.Navigate().GoToUrl("http://www.google.com");
-            driver.FindElement(By.ClassName("gLFyf")).SendKeys("google");
-            Thread.Sleep(500);
-            driver.FindElement(By.ClassName("gLFyf")).SendKeys(Keys.Enter);
-            var text = driver.FindElement(By.ClassName("e2BEnf")).Text;
-            Assert.That(text, Is.EqualTo("Главные новости").After(500));
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("budnikov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Мужской");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var text = driver.FindElement(By.ClassName("flashes")).Text;
+            Assert.That(text == "Спасибо за регистрацию!");
         }
+       
+        [Test]
+        public void Form_ShouldNotAcceptShortLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("bud");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Мужской");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Field must be between 4 and 24 characters long.");
+        }
+        
+        [Test]
+    public void Form_ShouldNotAcceptLongLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("sonyasonyasonyasonyasonya");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Мужской");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Field must be between 4 and 24 characters long.");
+        }
+        
+        [Test]
+        public void Form_ShouldNotAcceptSymbolsInLogin()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("sonya&");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Мужской");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Invalid input.");
+        } 
+        
+        [Test]
+        public void Form_ShouldNotAcceptInvalidEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("budnikov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("q@qq.q");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Invalid email address.");
+        }
+        
+        [Test]
+        public void Form_ShouldNotAcceptShortAndInvalidEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("budnikov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("q@q.q");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error.Contains("Field must be between 6 and 35 characters long"));
+        }
+        
+        
+        [Test]
+        public void Form_ShouldNotAcceptLongEmail()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("budnikov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("qqqqqqqq@qq.q");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("123");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Invalid email address.");
+        }
+        
+        [Test]
+        public void Password_ShouldMatch()
+        {
+            driver.Navigate().GoToUrl("https://qa-course.kontur.host/training/final");
+            driver.FindElement(By.Id("student")).SendKeys("Ekhlakova Sonya");
+            driver.FindElement(By.Id("username")).SendKeys("budnikov");
+            var sex = driver.FindElement(By.Id("sex"));
+            var selectSex = new SelectElement(sex);
+            selectSex.SelectByText("Male");
+            driver.FindElement(By.Id("email")).SendKeys("a@a.ru");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("confirm")).SendKeys("12");
+            var acceptTos = driver.FindElement(By.Id("accept_tos"));
+            acceptTos.Click();
+           
+            driver.FindElement(By.CssSelector("input[value=Register]")).Click();
+ 
+            var error = driver.FindElement(By.ClassName("errors")).Text;
+            Assert.That(error == "Passwords must match");
+        }
+        
     }
 }
